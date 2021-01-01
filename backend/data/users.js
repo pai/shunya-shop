bcf3a5749198307f04c68e44f45bba385414540d
iaps <pai.santosh@gmail.com>
2021-01-01 16:02:39 +0530

    added sample data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name = "Admin user",
+        email = "admin@sevenport.io",
+        password = bcrypt.hashSync('123456', 10),
+        isAdmin = true
+    },
+    {
+        name = "John Galt",
+        email = "john@sevenport.io",
+        password = bcrypt.hashSync('123456', 10),
+    
+    },
+    {
+        name = "Jane Galt",
+        email = "jane@sevenport.io",
+        password = bcrypt.hashSync('123456', 10),
+
+    },
+]
+
+export default users
