02f23cea08830b6d3f4e4eb0267ed7b4c718bbf3
iaps <pai.santosh@gmail.com>
2021-01-04 09:43:42 +0530

    added payment screen and save payment method

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -17,7 +17,7 @@ const ShippingScreen = ({ history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault()
-    dispatch(saveShippingAddress(address, city, postalCode, country))
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
     history.push('/payment')
   }
 
