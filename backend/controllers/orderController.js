44212aada0443acbc1e27f88eb4463f09c980d8f
iaps <pai.santosh@gmail.com>
2021-01-04 14:33:00 +0530

    updated paid endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
+import { privateDecrypt } from 'crypto'
 
 // @desc    Create new Order
 // @route   GET /api/orders
@@ -53,4 +54,27 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems, getOrderById }
+// @desc    Update Order to Paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }
+
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById, updateOrderToPaid }
