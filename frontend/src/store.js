d665901eeebdf00d69ea5bb5166c2dff44ab480a
iaps <pai.santosh@gmail.com>
2021-01-06 14:35:31 +0530

    added order list

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -24,6 +24,7 @@ import {
   orderDetailsReducer,
   orderPayReducer,
   orderListMyReducer,
+  orderListReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -44,6 +45,7 @@ const reducer = combineReducers({
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  orderList: orderListReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
